3ca69201b1c3cd1f8f03d08c2e70c229a756149e
freekiller201020 <htc.burchenko@gmail.com>
2019-09-27 19:16:59 +0300

    Update webhook.php

--- bot/webhook.php
+++ bot/webhook.php
@@ -13,6 +13,13 @@ $dbconn = pg_connect($query) or die('Не удалось соединиться:
 
 $token = "681634726:AAHafNwa8T3LXlezmIAUH-JjBGrI0qU-lfY";
 $bot = new \TelegramBot\Api\Client($token);
+
+// Не понял([a-z0-9]*)
+$bot->command('([a-z0-9]*)', function ($message) use ($bot) {
+	$answer = 'Простите, кажется я вас не понял. Введите "/help" что бь посмотреть что я умею.';
+	$bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
 // команда для start
 $bot->command('start', function ($message) use ($bot) {
 	$query = "INSERT INTO users (id) values ({$message->getChat()->getId()});\n";
