a7d1f8a260c7f6a315d5afa3ecee2553253ce201
freekiller201020 <htc.burchenko@gmail.com>
2019-10-20 19:56:13 +0300

    Update webhook.php

--- bot/webhook.php
+++ bot/webhook.php
@@ -70,35 +70,35 @@ $bot = new \TelegramBot\Api\Client($token);
 // });
 
 // Обработка кнопок у сообщений
-$bot->on(function ($update) use ($bot, $callback_loc, $find_command) {
-	$callback = $update->getCallbackQuery();
-	$message = $callback->getMessage();
-	$chatId = $message->getChat()->getId();
-	$data = $callback->getData();
+// $bot->on(function ($update) use ($bot, $callback_loc, $find_command) {
+// 	$callback = $update->getCallbackQuery();
+// 	$message = $callback->getMessage();
+// 	$chatId = $message->getChat()->getId();
+// 	$data = $callback->getData();
 
-	$mtext = $data;
-	$cid = $message->getChat()->getId();
-	$user = $message->getFrom()->getId();
-	$query = "INSERT INTO messages_history (timemark, message, chat_id, user_id) values (current_timestamp,'$mtext',$cid,$user);\n";
-	$result = pg_query($query) or $answer = 'Не удалось соединиться: ' . pg_last_error();
+// 	$mtext = $data;
+// 	$cid = $message->getChat()->getId();
+// 	$user = $message->getFrom()->getId();
+// 	$query = "INSERT INTO messages_history (timemark, message, chat_id, user_id) values (current_timestamp,'$mtext',$cid,$user);\n";
+// 	$result = pg_query($query) or $answer = 'Не удалось соединиться: ' . pg_last_error();
 
-	if ($data == "yes_start") {
-		$bot->sendMessage($message->getChat()->getId(), "Отлично! Напишите пожалуста свой игровой ник, что бы получать больше персональной информации ;)");
-		$bot->answerCallbackQuery($callback->getId(), "yes", false);
-	}
-	if ($data == "no_start") {
-		$bot->sendMessage($chatId, "Обидно...");
-		$bot->answerCallbackQuery($callback->getId(), "no", false); // можно отослать пустое, чтобы просто убрать "часики" на кнопке
-	}
+// 	if ($data == "yes_start") {
+// 		$bot->sendMessage($message->getChat()->getId(), "Отлично! Напишите пожалуста свой игровой ник, что бы получать больше персональной информации ;)");
+// 		$bot->answerCallbackQuery($callback->getId(), "yes", false);
+// 	}
+// 	if ($data == "no_start") {
+// 		$bot->sendMessage($chatId, "Обидно...");
+// 		$bot->answerCallbackQuery($callback->getId(), "no", false); // можно отослать пустое, чтобы просто убрать "часики" на кнопке
+// 	}
 
-}, function ($update) {
-	$callback = $update->getCallbackQuery();
-	if (is_null($callback) || !strlen($callback->getData())) {
-		return false;
-	}
+// }, function ($update) {
+// 	$callback = $update->getCallbackQuery();
+// 	if (is_null($callback) || !strlen($callback->getData())) {
+// 		return false;
+// 	}
 
-	return true;
-});
+// 	return true;
+// });
 
 $bot->on(function ($Update) use ($bot) {
 
@@ -115,8 +115,19 @@ $bot->on(function ($Update) use ($bot) {
 
 		$bot->sendPhoto($message->getChat()->getId(), $pic);
 	}
-	if (mb_stripos($mtext, "власть советам") !== false) {
-		$bot->sendMessage($message->getChat()->getId(), "Смерть богатым!");
+	if (mb_stripos($mtext, "Да!") !== false) {
+		$answer = 'Отлично! Вы хотели бы получать персональные уведомления';
+		$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(
+			[
+				[
+					["text" => "Да хочу!"],
+					["text" => "Нет, спасибо."],
+				],
+			]
+			, true, true);
+
+		$bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
+		// $bot->sendMessage($message->getChat()->getId(), "Отлично! Напишите, пожалуста, свой игровой ник, что бы получать больше персональной информации ;)");
 	}
 }, function ($message) use ($name) {
 	return true; // когда тут true - команда проходит
@@ -166,17 +177,16 @@ function Start($message, $bot) {
 	}
 	$answer = 'Добро пожаловать ' . $name . '!';
 	$bot->sendMessage($message->getChat()->getId(), $answer);
-	$answer = 'Вы состаите в каком-нибудь клане?';
-	$keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+	$answer = 'Вы состоите в каком-нибудь клане?';
+	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(
 		[
 			[
-				['callback_data' => 'yes_start', 'text' => 'Да'],
-			],
-			[
-				['callback_data' => 'no_start', 'text' => 'Нет'],
+				["text" => "Да!"],
+				["text" => "Нет :("],
 			],
 		]
-	);
+		, true, true);
 
-	$bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);}
+	$bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
+}
 ?>
