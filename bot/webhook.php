0174b9c9f67f1b2a2ab908f3a0c5bd056293f8e8
freekiller201020 <htc.burchenko@gmail.com>
2019-10-20 16:30:51 +0300

    Update webhook.php

--- bot/webhook.php
+++ bot/webhook.php
@@ -92,6 +92,44 @@ $bot->command('test', function ($message) use ($bot) {
 	$bot->sendMessage($message->getChat()->getId(), $answer);
 });
 
+// Кнопки у сообщений
+$bot->command("ibutton", function ($message) use ($bot) {
+	$keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+		[
+			[
+				['callback_data' => 'data_test', 'text' => 'Answer'],
+				['callback_data' => 'data_test2', 'text' => 'ОтветЪ'],
+			],
+		]
+	);
+
+	$bot->sendMessage($message->getChat()->getId(), "тест", false, null, null, $keyboard);
+});
+
+// Обработка кнопок у сообщений
+$bot->on(function ($update) use ($bot, $callback_loc, $find_command) {
+	$callback = $update->getCallbackQuery();
+	$message = $callback->getMessage();
+	$chatId = $message->getChat()->getId();
+	$data = $callback->getData();
+
+	if ($data == "data_test") {
+		$bot->answerCallbackQuery($callback->getId(), "This is Ansver!", true);
+	}
+	if ($data == "data_test2") {
+		$bot->sendMessage($chatId, "Это ответ!");
+		$bot->answerCallbackQuery($callback->getId()); // можно отослать пустое, чтобы просто убрать "часики" на кнопке
+	}
+
+}, function ($update) {
+	$callback = $update->getCallbackQuery();
+	if (is_null($callback) || !strlen($callback->getData())) {
+		return false;
+	}
+
+	return true;
+});
+
 $bot->run();
 pg_close($dbconn);
 
