ec136b151b117c2f2767e48afeb136feb25f31cb
freekiller201020 <htc.burchenko@gmail.com>
2019-11-05 18:05:13 +0300

    Update api.php

--- api/api.php
+++ api/api.php
@@ -38,7 +38,7 @@ $config = json_decode($file, true);
 // print_r($config);
 $query = "host={$config['host']} dbname={$config['dbname']} user={$config['user']} password={$config['password']}";
 // $dbconn = pg_pconnect($query) or die('Не удалось соединиться: ' . pg_last_error());
-$dbconn = pg_connect($query) or die('Не удалось соединиться: ' . pg_last_error());
+$dbconn = pg_connect($query) or die('API Не удалось соединиться: ' . pg_last_error());
 // $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
 OnCall($_REQUEST, $config);
