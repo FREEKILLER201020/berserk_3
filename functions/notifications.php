1521f51cbf9a003cafb278350e335e3940a3c1ee
freekiller201020 <htc.burchenko@gmail.com>
2019-11-07 18:35:23 +0300

    Update notifications.php

--- functions/notifications.php
+++ functions/notifications.php
@@ -112,7 +112,7 @@ $query = "SELECT * from bot_notification";
 $result = pg_query($query);
 $notifications = array();
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-	$tmp = new Notification_bot($line["id"], $line["chat_id"], $line["user_id"], $line["notification_type"], $line["pre_start_time"]);
+	$tmp = new NotificationBot($line["id"], $line["chat_id"], $line["user_id"], $line["notification_type"], $line["pre_start_time"]);
 	array_push($notifications, $tmp);
 }
 
