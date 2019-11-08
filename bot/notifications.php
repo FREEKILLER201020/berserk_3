e9400b508017cbb5fea4dc13ffd06e672965a84f
freekiller201020 <htc.burchenko@gmail.com>
2019-11-08 18:53:48 +0300

    Update notifications.php

--- bot/notifications.php
+++ bot/notifications.php
@@ -55,14 +55,14 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 	array_push($fights, $tmp);
 }
 
-// $tmp = new FightClassNot("Fireborn", "\"Берсерк\"", "Мир Кефки", "Лихолесье", "2019-11-07 19:00:00", "2019-11-07 16:05:00", "Fireborn", "2019-11-07 16:25:00", 6, 171, 171);
-// array_push($fights, $tmp);
-// $tmp = new FightClassNot("Fireborn", "\"Берсерк\"", "Мир Кефки", "Лихолесье", "2019-11-07 19:00:00", "2019-11-07 16:05:00", "Fireborn", "2019-11-07 16:26:10", 6, 171, 6);
-// array_push($fights, $tmp);
-// $tmp = new FightClassNot("\"Берсерк\"", "Fireborn", "Мир Кефки", "Лихолесье", "2019-11-07 19:00:00", "2019-11-07 16:05:00", "Fireborn", "2019-11-07 16:27:29", 171, 6, 171);
-// array_push($fights, $tmp);
-// $tmp = new FightClassNot("\"Берсерк\"", "Fireborn", "Мир Кефки", "Лихолесье", "2019-11-07 19:00:00", "2019-11-07 16:05:00", "Fireborn", "2019-11-07 16:28:59", 171, 6, 6);
-// array_push($fights, $tmp);
+$tmp = new FightClassNot("Fireborn", "\"Берсерк\"", "Мир Кефки", "Лихолесье", "2019-11-08 15:00:00", "2019-11-08 16:00:00", "Fireborn", "2019-11-08 16:01:00", 6, 171, 171);
+array_push($fights, $tmp);
+$tmp = new FightClassNot("Fireborn", "\"Берсерк\"", "Мир Кефки", "Лихолесье", "2019-11-08 15:00:00", "2019-11-08 16:00:00", "Fireborn", "2019-11-08 16:02:10", 6, 171, 6);
+array_push($fights, $tmp);
+$tmp = new FightClassNot("\"Берсерк\"", "Fireborn", "Мир Кефки", "Лихолесье", "2019-11-08 15:00:00", "2019-11-08 16:00:00", "Fireborn", "2019-11-08 16:03:29", 171, 6, 171);
+array_push($fights, $tmp);
+$tmp = new FightClassNot("\"Берсерк\"", "Fireborn", "Мир Кефки", "Лихолесье", "2019-11-08 15:00:00", "2019-11-08 16:00:00", "Fireborn", "2019-11-08 16:04:59", 171, 6, 6);
+array_push($fights, $tmp);
 print_r($fights);
 // exit();
 
@@ -128,7 +128,7 @@ foreach ($not as $user) {
 				$push->setToken('a5g19h6if4cdvvfrdw8n5najpm68rb');
 				$push->setUser($user->user_key);
 
-				$push->setTitle('Результат битвы!');
+				$push->setTitle('Результат битвы! time1: ' . $timestamp1 . ' time2: ' . $timestamp2);
 				if ($user->user_clan == $fight->winer_id) {
 					if ($user->user_clan == $fight->attacker_id) {
 						$push->setMessage($d . 'Ура! Победа! Мы отбили ' . $fight->to . ' у ' . $fight->defender);
