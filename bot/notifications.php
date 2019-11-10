2fd8ebc3772fda10d210d0bda42b420fb06ce858
freekiller201020 <htc.burchenko@gmail.com>
2019-11-11 00:18:13 +0300

    Update notifications.php

--- bot/notifications.php
+++ bot/notifications.php
@@ -90,36 +90,38 @@ print_r($not);
 foreach ($not as $user) {
 	foreach ($fights as $fight) {
 		if (($user->user_clan == $fight->attacker_id) || ($user->user_clan == $fight->defender_id)) {
-			$d = date('Y-m-d H:i:s');
-			echo $d . PHP_EOL;
-			$timestamp1 = strtotime($d);
-			$timestamp2 = strtotime($fight->resolved);
-			$d = round(($timestamp2 - $timestamp1) / 60);
-			echo $d . PHP_EOL;
+			if ($fight->ended == "") {
+				$d = date('Y-m-d H:i:s');
+				echo $d . PHP_EOL;
+				$timestamp1 = strtotime($d);
+				$timestamp2 = strtotime($fight->resolved);
+				$d = round(($timestamp2 - $timestamp1) / 60);
+				echo $d . PHP_EOL;
 
-			if ($d == $time) {
-				$push = new Pushover();
+				if ($d == $time) {
+					$push = new Pushover();
 
-				$push->setToken('a5g19h6if4cdvvfrdw8n5najpm68rb');
-				$push->setUser($user->user_key);
+					$push->setToken('a5g19h6if4cdvvfrdw8n5najpm68rb');
+					$push->setUser($user->user_key);
 
-				$push->setTitle('Скоро битва!');
-				if ($user->user_clan == $fight->attacker_id) {
-					$push->setMessage('Через ' . $time . ' минут начнется бой против ' . $fight->defender . ' за ' . $fight->to . ' (защищаемся)');
+					$push->setTitle('Скоро битва!');
+					if ($user->user_clan == $fight->attacker_id) {
+						$push->setMessage('Через ' . $time . ' минут начнется бой против ' . $fight->defender . ' за ' . $fight->to . ' (защищаемся)');
 
-				} else if ($user->user_clan == $fight->defender_id) {
-					$push->setMessage('Через ' . $time . ' минут начнется бой против ' . $fight->attacker . ' за ' . $fight->to . ' (атакуем)');
+					} else if ($user->user_clan == $fight->defender_id) {
+						$push->setMessage('Через ' . $time . ' минут начнется бой против ' . $fight->attacker . ' за ' . $fight->to . ' (атакуем)');
 
+					}
+					// $push->setUrl('http://chris.schalenborgh.be/blog/');
+					// $push->setUrlTitle('cool php blog');
+					// $push->setDevice('pixel2xl');
+					$push->setPriority(0);
+					// $push->setRetry(60); //Used with Priority = 2; Pushover will resend the notification every 60 seconds until the user accepts.
+					// $push->setExpire(3600); //Used with Priority = 2; Pushover will resend the notification every 60 seconds for 3600 seconds. After that point, it stops sending notifications.
+					// $push->setTimestamp(time());
+					print_r($push);
+					$push->send();
 				}
-				// $push->setUrl('http://chris.schalenborgh.be/blog/');
-				// $push->setUrlTitle('cool php blog');
-				// $push->setDevice('pixel2xl');
-				$push->setPriority(0);
-				// $push->setRetry(60); //Used with Priority = 2; Pushover will resend the notification every 60 seconds until the user accepts.
-				// $push->setExpire(3600); //Used with Priority = 2; Pushover will resend the notification every 60 seconds for 3600 seconds. After that point, it stops sending notifications.
-				// $push->setTimestamp(time());
-				print_r($push);
-				$push->send();
 			}
 		}
 		if (($user->user_clan == $fight->attacker_id) || ($user->user_clan == $fight->defender_id)) {
@@ -241,28 +243,30 @@ foreach ($notifications as $notification) {
 	if ($notification->type == 1) {
 		// if ($notification->chat_id == 249857309) {
 		foreach ($fights as $fight) {
-			if (($notification->clan_id == $fight->attacker_id) || ($notification->clan_id == $fight->defender_id)) {
-				$d = date('Y-m-d H:i:s');
-				echo $d . PHP_EOL;
-				$timestamp1 = strtotime($d);
-				$timestamp2 = strtotime($fight->resolved);
-				$d = round(($timestamp2 - $timestamp1) / 60);
-				echo $d . PHP_EOL;
+			if ($fight->ended == "") {
+				if (($notification->clan_id == $fight->attacker_id) || ($notification->clan_id == $fight->defender_id)) {
+					$d = date('Y-m-d H:i:s');
+					echo $d . PHP_EOL;
+					$timestamp1 = strtotime($d);
+					$timestamp2 = strtotime($fight->resolved);
+					$d = round(($timestamp2 - $timestamp1) / 60);
+					echo $d . PHP_EOL;
 
-				if ($d == $notification->time) {
-					$answer = "Скоро битва!";
-					echo $answer;
-					$bot->sendMessage($notification->chat_id, $answer, null, null, null, null);
+					if ($d == $notification->time) {
+						$answer = "Скоро битва!";
+						echo $answer;
+						$bot->sendMessage($notification->chat_id, $answer, null, null, null, null);
 
-					if ($notification->clan_id == $fight->attacker_id) {
-						$answer = 'Через ' . $time . ' минут начнется бой против ' . $fight->defender . ' за ' . $fight->to . ' (защищаемся)';
+						if ($notification->clan_id == $fight->attacker_id) {
+							$answer = 'Через ' . $time . ' минут начнется бой против ' . $fight->defender . ' за ' . $fight->to . ' (защищаемся)';
 
-					} else if ($notification->clan_id == $fight->defender_id) {
-						$answer = 'Через ' . $time . ' минут начнется бой против ' . $fight->attacker . ' за ' . $fight->to . ' (атакуем)';
+						} else if ($notification->clan_id == $fight->defender_id) {
+							$answer = 'Через ' . $time . ' минут начнется бой против ' . $fight->attacker . ' за ' . $fight->to . ' (атакуем)';
+
+						}
+						$bot->sendMessage($notification->chat_id, $answer, null, null, null, null);
 
 					}
-					$bot->sendMessage($notification->chat_id, $answer, null, null, null, null);
-
 				}
 			}
 		}
