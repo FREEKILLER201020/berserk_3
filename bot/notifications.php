e83dd8da29126bc6fbb2ad5e6dca876ec006f906
freekiller201020 <htc.burchenko@gmail.com>
2019-11-15 01:08:51 +0300

    Update notifications.php

--- bot/notifications.php
+++ bot/notifications.php
@@ -303,6 +303,8 @@ foreach ($notifications as $notification) {
 			$timestamp1 = strtotime($d);
 			$timestamp2 = strtotime($ttime);
 			$d = round(($timestamp1 - $timestamp2) / 60);
+			echo PHP_EOL . "NOTIFICATION 4" . PHP_EOL . $timestamp1 . PHP_EOL;
+			echo PHP_EOL . "NOTIFICATION 4" . PHP_EOL . $timestamp2 . PHP_EOL;
 			echo PHP_EOL . "NOTIFICATION 4" . PHP_EOL . $d . PHP_EOL;
 
 			if (($d >= 0) && ($d < 1)) {
