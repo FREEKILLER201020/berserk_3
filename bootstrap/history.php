ed42ffd6364eaea212061cea2d44e5a89b0178d1
freekiller201020 <htc.burchenko@gmail.com>
2021-02-04 23:30:02 +0300

    ajax update

--- bootstrap/history.php
+++ bootstrap/history.php
@@ -9,7 +9,7 @@ require "common.php";
 <script src="../js/index.js"></script>
 <script src="../js/script.js"></script>
 <script src="../js/functions.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
 
 
