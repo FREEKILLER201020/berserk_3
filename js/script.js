37da0df4f9fa347c097d37ed6fabef01455dcf44
freekiller201020 <htc.burchenko@gmail.com>
2021-02-05 00:10:45 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,42 @@
+        // var checkbox = document.querySelector('input[name=theme]');
+        // var text = document.querySelector('text[id=moon]');
+        // if (matchMedia('(prefers-color-scheme: dark)').matches){
+        //     if (readCookie('data-theme')=='light'){
+        //         // checkbox.checked=true;
+        //         document.documentElement.setAttribute('data-theme', 'light')
+        //         text.textContent="🌑"
+        //     }
+        //     else{
+        //         checkbox.checked=true;
+        //         document.documentElement.setAttribute('data-theme', 'dark') 
+        //         text.textContent="☀️"
+        //     }
+        // }
+        // checkbox.addEventListener('change', function() {
+        //     if(this.checked) {
+        //         trans()
+        //         document.documentElement.setAttribute('data-theme', 'dark')
+        //         // <?php
+        //         //     session_start();
+        //         //     $_SESSION['data-theme']="dark";
+        //         // ?>
+        //         writeCookie('data-theme', 'dark', 3);
+        //         text.textContent="☀️"
+        //     } else {
+        //         trans()
+        //         document.documentElement.setAttribute('data-theme', 'light')
+        //         // <?php
+        //         //     session_start();
+        //         //     $_SESSION['data-theme']="light";
+        //         // ?>
+        //         writeCookie('data-theme', 'light', 3);
+        //         text.textContent="🌑"
+        //     }
+        // })
+
+        // let trans = () => {
+        //     document.documentElement.classList.add('transition');
+        //     window.setTimeout(() => {
+        //         document.documentElement.classList.remove('transition')
+        //     }, 1000)
+        // }
